d9bfdfc4d7bedf7a6a12a3030ba5041dd7903aff
EvvTim <evgeniytim94@gmail.com>
2021-12-25 13:33:57 +0100

    feat: add App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import OutputSection from "./components/OutputSection/OutputSection";
+import InputSection from "./components/InputSection/InputSection";
+import useData from "./hooks/useData";
+import useReloadData from "./hooks/useReloadData";
+import useStdDeviation from "./hooks/useStdDeviation";
+import useMedian from "./hooks/useMedian";
+import useMean from "./hooks/useMean";
 
 function App() {
+    const [data, changeDataHandler, addValue] = useData()
+    const [reloadData1234Handler, reloadData4321Handler, reloadData] = useReloadData(data)
+    const stdDeviation = useStdDeviation(reloadData)
+    const median = useMedian(reloadData)
+    const mean = useMean(reloadData)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <OutputSection stdDeviation={stdDeviation} median={median} mean={mean}/>
+        <InputSection
+            reloadData1234={reloadData1234Handler}
+            reloadData4321={reloadData4321Handler}
+            dataChangeHandler={changeDataHandler}
+            addValue={addValue}
+        />
+      </div>
   );
 }
 
